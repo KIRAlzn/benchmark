c566f7fd0b734d601834be1da50df447b8b025a6
chengduoZH <zhaochengduo@163.com>
2018-06-21 22:31:03 +0800

    Add test option

--- fluid/ResNet_50/train_resnet.py
+++ fluid/ResNet_50/train_resnet.py
@@ -202,16 +202,11 @@ def train_parallel_exe(args):
 
     train_exe = fluid.ParallelExecutor(
         loss_name=avg_cost.name,
-        main_program=fluid.default_main_program(),
+        main_program=test_program
+        if args.with_test else fluid.default_main_program(),
         use_cuda=True if args.use_gpu else False,
         build_strategy=build_strategy,
         exec_strategy=exec_strategy)
-
-    test_exe = fluid.ParallelExecutor(
-        use_cuda=True if args.use_gpu else False,
-        main_program=test_program,
-        share_vars_from=train_exe,
-        build_strategy=build_strategy)
 
     # Prepare Data
     feeder = fluid.DataFeeder(place=place, feed_list=[image, label])
@@ -256,15 +251,6 @@ def train_parallel_exe(args):
             print("iter=%d, cost=%s, elapse=%f, img/sec=%f" %
                   ((batch_id + 1), np.array(cost_val[0]), step_time,
                    img_count / step_time))
-
-            if args.with_test:
-                test_start = time.time()
-                test_loss, = test_exe.run([avg_cost.name], feed=feed_data)
-                test_end = time.time()
-                step_time = test_end - test_start
-                print("iter=%d, test_cost=%s, elapse=%f, img/sec=%f" %
-                      ((batch_id + 1), np.array(cost_val[0]), step_time,
-                       args.batch_size / step_time))
 
             img_count = 0
             train_start = time.time()
